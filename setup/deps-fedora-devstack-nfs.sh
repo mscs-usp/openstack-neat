619a250283bd2e6078dd0e5705110d95a075638e
Albert De La Fuente <vonpupp@gmail.com>
2013-09-25 18:58:26 -0300

    Added fedora-devstack-nfs dependencies

--- setup/deps-fedora-devstack-nfs.sh
+++ setup/deps-fedora-devstack-nfs.sh
@@ -14,7 +14,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-sudo yum install -y nfs-common nfs-utils
+sudo yum install -y nfs-utils
 sudo service rpcbind start
 sudo mkdir /var/lib/nova
 sudo mkdir /var/lib/nova/instances
